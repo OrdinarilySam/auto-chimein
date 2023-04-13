2b87cb28788270eaeeb618dfc25694d208784037
OrdinarilySam <56322367+OrdinarilySam@users.noreply.github.com>
2023-04-13 00:37:47 -0500

    updates checkbox to reflect localstorage

--- popup.js
+++ popup.js
@@ -1,41 +1,46 @@
-const discordPingsChk = document.getElementById("discordPingsChk")
-const webhookUrlInp = document.getElementById("webhookUrlInp")
-const optionsForm = document.getElementById("optionsForm")
-const urlStatus = document.getElementById("urlStatus")
+const discordPingsChk = document.getElementById("discordPingsChk");
+const webhookUrlInp = document.getElementById("webhookUrlInp");
+const optionsForm = document.getElementById("optionsForm");
+const urlStatus = document.getElementById("urlStatus");
 
 const regex = /https:\/\/discord\.com\/api\/webhooks\/\d+\/[\w-]+/;
 
-let values = {pings: true, url: null}
-if(localStorage.getItem("autoChimeValues")){
-  values = JSON.parse(localStorage.getItem("autoChimeValues"))
-  if(regex.test(values.url)){
-    urlStatus.textContent = "URL found"
-  }else{
-    urlStatus.textContent = "No URL"
+let values = { pings: true, url: null };
+if (localStorage.getItem("autoChimeValues")) {
+  values = JSON.parse(localStorage.getItem("autoChimeValues"));
+  if (regex.test(values.url)) {
+    urlStatus.textContent = "URL found";
+  } else {
+    urlStatus.textContent = "No URL";
+  }
+  if (values.pings) {
+    discordPingsChk.checked = true;
+  } else {
+    discordPingsChk.checked = false;
   }
 }
 
-function updateLocalStorage(){
-  localStorage.setItem("autoChimeValues", JSON.stringify(values))
+function updateLocalStorage() {
+  localStorage.setItem("autoChimeValues", JSON.stringify(values));
 }
 
 discordPingsChk.addEventListener("change", (event) => {
-  if(event.target.checked){
+  if (event.target.checked) {
     values.pings = true;
-  }else{
+  } else {
     values.pings = false;
   }
   updateLocalStorage();
-})
+});
 
 optionsForm.addEventListener("submit", (event) => {
   event.preventDefault();
-  if(regex.test(webhookUrlInp.value)){
-    values.url = webhookUrlInp.value
+  if (regex.test(webhookUrlInp.value)) {
+    values.url = webhookUrlInp.value;
     updateLocalStorage();
-    urlStatus.textContent = "URL updated"
-  }else{
-    urlStatus.textContent = "Failed, not a valid URL"
+    urlStatus.textContent = "URL updated";
+  } else {
+    urlStatus.textContent = "Failed, not a valid URL";
   }
-  webhookUrlInp.value = ""
-})
+  webhookUrlInp.value = "";
+});
